1a12a5d10d2f1a3f17f25ed1f821d9b492c3d37d
Olivier Leroy <leroy.oli@gmail.com>
2021-06-02 22:21:31 +0200

    un peu de mise en page

--- explo_metadat_inv.R
+++ explo_metadat_inv.R
@@ -1,21 +1,33 @@
-# une explo rapide des metadata des inv.
-# 2021
+# Objectif: une premiere exploration des bases de données en open data des arbres
+# en france
+# Data :la source est un fichier tentant une première synthèse
+# Date: Huin2021
+
+# I Chargement des données et package ###########
+
+pkgs <-  c("openxlsx","ggplot2")
+
+inst <- lapply(pkgs, 
+               library, # piste amelioration inclure une verif et si non install
+               character.only = TRUE)
 
 metada_inv <- openxlsx::read.xlsx("intermed/tentative_synthesev1.xlsx")
 
+# correction de la date 
 metada_inv$year <- substr(metada_inv$Date_publication, 
                           nchar(metada_inv$Date_publication) - 3,
                           nchar(metada_inv$Date_publication))
 
 # si pas de date de publication, date de modification 
-metada_inv$Date_publication[is.na(metada_inv$Date_publication)] <- metada_inv$Date_modif[is.na(metada_inv$Date_publication)] 
+metada_inv$Date_publication[ is.na(metada_inv$Date_publication) ] <- metada_inv$Date_modif[ is.na(metada_inv$Date_publication) ] 
 
+# première version d'exploration
 barplot(
         table(metada_inv$year),
         las = 2,
         main = "Diagramme en baton du nombre de jeux de données / an")
 
-# C'est pas mal mais l axe temporel est pas top
+# C'est pas mal mais l'axe temporel est pas top
 # en base pour l'exercice
 # on va generer un tableau avec toutes les années
 # assigner 0
